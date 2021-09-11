c2170b90d7e229e65ce83e61589f0f1d5ead71cb
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-11 14:52:27 +0900

    nest generateで簡単にcrudが完成した！なんやねんこれ〜！！！

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { SetupModule } from './setup/setup.module';
 
 @Module({
-  imports: [BookModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    BookModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    SetupModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
